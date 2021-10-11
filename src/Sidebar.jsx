725e8bf129d6f80e0814759f9831efb375952865
nishantc999 <nishantc999@gmail.com>
2021-10-11 13:50:50 +0530

    react2

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Sidebar = () => {
     return (
       <>
       <input type="checkbox" id="sidebar-toggle" className="sidebar-toggle"/>
       <label htmlFor="sidebar-toggle" className="bg-dark mt-1 w-100"><i class="fas fa-toggle-on fa-2x text-white"></i></label>
         <div class="sidebar-div">
-            <ul class="sidebar-ul">
-                <li class="sidebar-li">Home</li>
-                <li class="sidebar-li">Categories</li>
-                <li class="sidebar-li">About</li>
+            <ul class="sidebar-ul">8449565890
+                <li class="sidebar-li"><Link class="link" to="/Ecommerce/">Home</Link></li>
+                <li class="sidebar-li"><Link class="link" to="/Ecommerce/">Categories</Link></li>
+                <li class="sidebar-li"><Link class="link" to="/Ecommerce/">Brands</Link></li>
             </ul>
         </div>
       </>
     )
 }
 
-export default Sidebar
+export default Sidebar;
